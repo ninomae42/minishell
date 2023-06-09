6602869cac549113351bc027a1a12956dc15b7fe
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-09 23:40:20 +0900

    add tokenizer related data types

---
+++ srcs/tokenizer.h
@@ -0,0 +1,28 @@
+#ifndef TOKENIZER_H
+# define TOKENIZER_H
+# include "minishell.h"
+
+typedef enum e_token_kind	t_token_kind;
+enum e_token_kind
+{
+	TK_EOF,
+	TK_WORD,
+};
+
+typedef struct s_token_node	t_token_node;
+struct s_token_node
+{
+	t_token_kind	kind;
+	char			*literal;
+	t_token_node	*next;
+};
+
+typedef struct s_token	t_token;
+struct s_token
+{
+	t_token_node	*head;
+	t_token_node	*tail;
+	size_t			size;
+};
+
+#endif
