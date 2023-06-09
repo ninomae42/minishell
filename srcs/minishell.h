3114f9dc5c6f11a3516973cc26a98bd968dfc3d8
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-09 20:14:11 +0900

    add readline loop

---
+++ srcs/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
+
+#endif
