82df87c5f808295966dc4f9939244396f6ead558
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-10 19:20:23 +0900

    add ft_fatal
    致命的で回収するのが実質難しいエラーを受け取り、プログラムを終了する関数を追加

--- srcs/err_main.c
+++ srcs/err_main.c
@@ -5,3 +5,9 @@ void	err_puterr(char *err_msg)
 	ft_putstr_fd(ERR_PREFIX, STDERR_FILENO);
 	ft_putendl_fd(err_msg, STDERR_FILENO);
 }
+
+void	ft_fatal(char *func_name)
+{
+	perror(func_name);
+	exit(EXIT_FAILURE);
+}
--- srcs/ft_err.h
+++ srcs/ft_err.h
@@ -1,6 +1,8 @@
 #ifndef FT_ERR_H
 # define FT_ERR_H
 # include <errno.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 # include "libft.h"
 
@@ -8,5 +10,6 @@
 
 // err_main.c
 void	err_puterr(char *err_msg);
+void	ft_fatal(char *func_name);
 
 #endif
