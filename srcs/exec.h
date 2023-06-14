6c8a4c9e9b9747baedc1ee66ca4803973678846e
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-15 08:44:36 +0900

    [clear] execパッケージのリセット

--- srcs/exec.h
+++ srcs/exec.h
@@ -4,35 +4,6 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-typedef struct s_redirect	t_redirect;
-struct s_redirect
-{
-	bool	need_backup_in;
-	int		default_in_fd;
-	bool	need_backup_out;
-	int		default_out_fd;
-};
-
-typedef struct s_cmd	t_cmd;
-struct s_cmd
-{
-	size_t		argc;
-	char		**argv;
-	char		**environ;
-	char		*exec_path;
-	t_redirect	redirect;
-};
-
-// exec_cmd.c
-t_cmd	*new_cmd(void);
-void	cmd_destroy(t_cmd *cmd);
-
-// exec_main.c
-int		exec_cmd(t_ast *ast);
-
-// exec_args.c
-size_t	count_argc(t_ast_node *node);
-char	**dup_argv(t_ast_node *node, size_t argc);
 
 // exec_path.c
 char	*cmd_get_binary_path(char *filename);;
--- srcs/exec_main.c
+++ srcs/exec_main.c
@@ -1,45 +1,11 @@
 #include "exec.h"
 
-extern char	**environ;
-
-void	set_argv_and_redirect(t_cmd *cmd, t_ast_node *node);
-
-t_cmd	*build_simple_command(t_ast_node *node)
-{
-	t_cmd	*cmd;
-
-	cmd = new_cmd();
-	cmd->argc = count_argc(node);
-	// cmd->argv = dup_argv(node, cmd->argc);
-	cmd->argv = (char **)malloc(sizeof(char *) * (cmd->argc + 1));
-	set_argv_and_redirect(cmd, node);
-	cmd->environ = environ;
-	cmd->exec_path = cmd_get_binary_path(cmd->argv[0]);
-	return (cmd);
-}
-
-t_cmd	*make_cmd(t_ast *ast)
-{
-	t_cmd	*cmd;
-
-	if (ast->root->kind == ND_SIMPLE_COMMAND)
-		cmd = build_simple_command(ast->root->child);
-	else
-		cmd = NULL;
-	return (cmd);
-}
-
-int	exec_simple_command(t_cmd *cmd);
 
 int	exec_cmd(t_ast *ast)
 {
 	int		status;
-	t_cmd	*cmd;
 
+	(void)ast;
 	status = 0;
-	cmd = make_cmd(ast);
-	if (cmd->exec_path == NULL)
-		return (127);
-	status = exec_simple_command(cmd);
 	return (status);
 }
