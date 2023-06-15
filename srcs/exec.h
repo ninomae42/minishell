4924c67f528ee01adb6056b541583c60c7b63224
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-15 09:27:16 +0900

    comamnd node 構造体とその操作関数を追加

--- srcs/exec.h
+++ srcs/exec.h
@@ -4,6 +4,19 @@
 # include <fcntl.h>
 # include <unistd.h>
 
+typedef struct s_cmd_node	t_cmd_node;
+struct s_cmd_node
+{
+	t_ast_node	*node;
+	size_t		argc;
+	char		**argv;
+	char		**environ;
+	char		*binary_path;
+	t_cmd_node	*next;
+};
+
+t_cmd_node	*new_cmd_node(t_ast_node *node);
+void		destroy_cmd_node(t_cmd_node *cmd);
 
 // exec_path.c
 char	*cmd_get_binary_path(char *filename);;
--- srcs/exec_main.c
+++ srcs/exec_main.c
@@ -1,5 +1,26 @@
 #include "exec.h"
 
+t_cmd_node	*new_cmd_node(t_ast_node *node)
+{
+	t_cmd_node	*cmd;
+
+	cmd = (t_cmd_node *)malloc(sizeof(t_cmd_node));
+	if (cmd == NULL)
+		ft_fatal("malloc");
+	cmd->node = node;
+	cmd->argc = 0;
+	cmd->argv = NULL;
+	cmd->environ = NULL;
+	cmd->binary_path = NULL;
+	cmd->next = NULL;
+	return (cmd);
+}
+
+void	destroy_cmd_node(t_cmd_node *cmd)
+{
+	free(cmd->argv);
+	free(cmd);
+}
 
 int	exec_cmd(t_ast *ast)
 {
