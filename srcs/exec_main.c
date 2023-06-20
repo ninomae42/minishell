48b69cbf3e9dbd5246e0858a2a80bc811d1e0830
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-21 06:38:07 +0900

    子プロセス内でパイプを接続する処理を実装
    現在のコマンドの位置に応じてコマンド接続の挙動を指定するset pipe state 関数と
    pipe modeに応じてコマンドを接続するconnect pipe 関数を追加

--- srcs/exec_main.c
+++ srcs/exec_main.c
@@ -158,6 +158,10 @@ void	exec_simple_command_child(t_cmd_node *cmd)
 	}
 }
 
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+#define PIPE_READ_WRITE 2
+
 void	exec_open_pipe(t_cmd_node *command)
 {
 	int	pipe_fd[2];
@@ -172,6 +176,37 @@ void	exec_close_pipe(t_cmd_node *command)
 {
 	close(command->pipe_read_fd);
 	close(command->pipe_write_fd);
+}
+
+void	exec_set_pipe_state(t_cmd_node *current, t_cmd_node *prev)
+{
+	if (current == prev)
+		current->pipe_mode = PIPE_WRITE;
+	else if (current->next == NULL)
+		current->pipe_mode = PIPE_READ;
+	else
+		current->pipe_mode = PIPE_READ_WRITE;
+}
+
+void	exec_connect_pipes(t_cmd_node *current, t_cmd_node *prev)
+{
+	if (current->pipe_mode == PIPE_WRITE)
+	{
+		dup2(current->pipe_write_fd, STDOUT_FILENO);
+		exec_close_pipe(current);
+	}
+	else if (current->pipe_mode == PIPE_READ_WRITE)
+	{
+		dup2(prev->pipe_read_fd, STDIN_FILENO);
+		dup2(current->pipe_write_fd, STDOUT_FILENO);
+		exec_close_pipe(prev);
+		exec_close_pipe(current);
+	}
+	else if (current->pipe_mode == PIPE_READ)
+	{
+		dup2(prev->pipe_read_fd, STDIN_FILENO);
+		exec_close_pipe(prev);
+	}
 }
 
 int	exec_cmd(t_ast *ast)
