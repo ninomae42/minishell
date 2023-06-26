626532a8bd804afcbeb7eab651b661357a1e5c14
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-27 02:33:35 +0900

    add double quote expander

--- srcs/expander.c
+++ srcs/expander.c
@@ -81,6 +81,47 @@ char	*expand_parameter(char **word)
 	return (value);
 }
 
+char	*duplicate_within_dquote(char **word)
+{
+	char	*iword;
+	char	*save_word;
+	char	*res;
+
+	iword = *word;
+	save_word = iword;
+	res = NULL;
+	while (iword && *iword && *iword != '"' && *iword != '$')
+		iword++;
+	res = ft_strndup(save_word, iword - save_word);
+	if (res == NULL)
+		err_fatal(errno);
+	*word = iword;
+	return (res);
+}
+
+char	*expand_double_quote(char **word)
+{
+	char	*iword;
+	char	*res;
+	char	*value;
+
+	iword = *word;
+	iword++;
+	res = NULL;
+	value = NULL;
+	while (*iword && *iword != '"')
+	{
+		if (*iword == '$')
+			value = expand_parameter(&iword);
+		else
+			value = duplicate_within_dquote(&iword);
+		res = concat_str(res, value);
+	}
+	iword++;
+	*word = iword++;
+	return (res);
+}
+
 char	*expand_word(char *word)
 {
 	char	*iword;
@@ -94,6 +135,8 @@ char	*expand_word(char *word)
 	{
 		if (*iword == '\'')
 			value = expand_single_quote(&iword);
+		else if (*iword == '"')
+			value = expand_double_quote(&iword);
 		else if (*iword == '$')
 			value = expand_parameter(&iword);
 		else
