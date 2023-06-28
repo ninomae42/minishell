26d3cee7bc899373f2cbb81c583b2b9f46c5478e
Takuma Shimizu <97785630+ninomae42@users.noreply.github.com>
2023-06-28 14:50:09 +0900

    fix builtin export to print sorted enviroment variables

--- srcs/builtin_export_print.c
+++ srcs/builtin_export_print.c
@@ -1,6 +1,65 @@
 #include "builtin.h"
 #include "expander.h"
 
+t_env_node	**copy_env_entries(t_env *env)
+{
+	t_env_node	**env_array;
+	t_env_node	*node;
+	size_t		i;
+
+	env_array = (t_env_node **)malloc(sizeof(t_env_node *) * (env->size));
+	if (env_array == NULL)
+		err_fatal(errno);
+	node = env->head;
+	i = 0;
+	while (node)
+	{
+		env_array[i] = new_env_node(node->name, node->value, NULL);
+		if (env_array[i] == NULL)
+			err_fatal(errno);
+		node = node->next;
+		i++;
+	}
+	return (env_array);
+}
+
+void	dealloc_env_entries(t_env_node **nodes, size_t num_of_entries)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < num_of_entries)
+	{
+		free(nodes[i]);
+		i++;
+	}
+	free(nodes);
+}
+
+void	sort_env_entries(t_env_node **entries, size_t num_of_entries)
+{
+	size_t		i;
+	size_t		j;
+	t_env_node	*tmp;
+
+	i = 0;
+	while (i < num_of_entries - 1)
+	{
+		j = i + 1;
+		while (j < num_of_entries)
+		{
+			if (ft_strcmp(entries[i]->name, entries[j]->name) > 0)
+			{
+				tmp = entries[i];
+				entries[i] = entries[j];
+				entries[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 void	print_env_node(t_env_node *node)
 {
 	char	*escaped_value;
@@ -20,16 +79,17 @@ void	print_env_node(t_env_node *node)
 
 int	export_print(void)
 {
-	t_env_node	*node;
-	t_env_node	*next;
+	t_env_node	**env_array;
+	size_t		i;
 
-	// TODO: sort environment node list
-	node = g_env->head;
-	while (node)
+	env_array = copy_env_entries(g_env);
+	sort_env_entries(env_array, g_env->size);
+	i = 0;
+	while (i < g_env->size)
 	{
-		next = node->next;
-		print_env_node(node);
-		node = next;
+		print_env_node(env_array[i]);
+		i++;
 	}
+	dealloc_env_entries(env_array, g_env->size);
 	return (EXIT_SUCCESS);
 }
